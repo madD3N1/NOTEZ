af1ace33192ffba56123fd0808de9471dd01b8d8
Daniel Kukhta <woodyw86rus@gmail.com>
2021-03-14 23:10:29 +0500

    Важные изменения в UserController. Переделан и частично реализован консольный интерфейс.

--- NOTEZ.BL/Controller/UserController.cs
+++ NOTEZ.BL/Controller/UserController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 
@@ -13,9 +14,37 @@ namespace NOTEZ.BL.Controller
     public class UserController
     {
         /// <summary>
-        /// Пользователь приложения.
+        /// Список пользователей приложения.
         /// </summary>
-        public User User { get; }
+        public List<User> Users { get; }
+
+        /// <summary>
+        /// Текущий пользователь.
+        /// </summary>
+        public User CurrentUser { get; set; }
+
+        /// <summary>
+        /// Флаг нового пользователя.
+        /// </summary>
+        public bool IsNewUser { get; set; } = false;
+
+        /// <summary>
+        /// Флаг списка пользователей.
+        /// </summary>
+        public bool IsEmptyListUsers { get; set; } = true;
+
+        /// <summary>
+        /// Создание нового контроллера пользователя.
+        /// </summary>
+        public UserController()
+        {
+            Users = GetUsersData();
+
+            if(Users.Count != 0)
+            {
+                IsEmptyListUsers = false;
+            }
+        }
 
         /// <summary>
         /// Создание нового контроллера пользователя.
@@ -28,38 +57,100 @@ namespace NOTEZ.BL.Controller
                 throw new ArgumentNullException("Имя пользователя не может быть пустым или null.", nameof(userName));
             }
 
-            User = new User(userName);
-        }
+            Users = GetUsersData();
 
-        /// <summary>
-        /// Получение данных пользователя.
-        /// </summary>
-        /// <returns> Пользователь приложения. </returns>
-        public UserController()
-        {
-            var formatter = new BinaryFormatter();
-
-            using (var fileStream = new FileStream("users.dat", FileMode.Open))
+            if (Users.Count != 0)
             {
-                if (formatter.Deserialize(fileStream) is User user)
-                {
-                    User = user;
-                }
-
-                // TODO: Что делать, если пользователя не прочитали?
+                SetCurrentUser(userName);
+            }
+            else
+            {
+                CreateUser(userName);
             }
         }
 
         /// <summary>
-        /// Сохранение данных пользователя.
+        /// Получение списка пользователей.
         /// </summary>
-        public void Save()
+        /// <returns> Список пользователей. </returns>
+        private List<User> GetUsersData()
         {
             var formatter = new BinaryFormatter();
 
             using (var fileStream = new FileStream("users.dat", FileMode.OpenOrCreate))
             {
-                formatter.Serialize(fileStream, User);
+                if(fileStream.Length != 0)
+                {
+                    if(formatter.Deserialize(fileStream) is List<User> users)
+                    {
+                        return users;
+                    }
+                    else
+                    {
+                        return new List<User>();
+                    }
+                }
+                else
+                {
+                    return new List<User>();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Поиск пользователя.
+        /// </summary>
+        /// <param name="userName"> Имя пользователя. </param>
+        /// <returns></returns>
+        public bool UserSearch(string userName)
+        {
+            if(!IsEmptyListUsers)
+            {
+                foreach(var user in Users)
+                {
+                    if(user.Name == userName)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Создание пользователя.
+        /// </summary>
+        /// <param name="userName"> Имя пользователя. </param>
+        public void CreateUser(string userName)
+        {      
+            CurrentUser = new User(userName);         
+            IsNewUser = true;
+            Users.Add(CurrentUser);
+            IsEmptyListUsers = false;
+            Save();
+        }
+
+        /// <summary>
+        /// Установка текущего пользователя.
+        /// </summary>
+        /// <param name="userName"> Имя пользователя. </param>
+        public void SetCurrentUser(string userName)
+        {
+            CurrentUser = Users.Find(u => u.Name == userName);
+            IsNewUser = false;
+        }
+
+        /// <summary>
+        /// Сохранение данных пользователей.
+        /// </summary>
+        private void Save()
+        {
+            var formatter = new BinaryFormatter();
+
+            using (var fileStream = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fileStream, Users);
             }          
         }
 
@@ -69,7 +160,7 @@ namespace NOTEZ.BL.Controller
         /// <param name="title"> Название блокнота. </param>
         public void CreateNotebook(string title)
         {
-            User.Notebooks.Add(new Notebook(title));
+            CurrentUser.Notebooks.Add(new Notebook(title));
         }
 
         /// <summary>
@@ -79,7 +170,7 @@ namespace NOTEZ.BL.Controller
         /// <param name="titleNote"> Название заметки. </param>
         public void CreateNote(string titleNotebook, string titleNote)
         {
-            foreach(var element in User.Notebooks)
+            foreach(var element in CurrentUser.Notebooks)
             {
                 if (element.Title == titleNotebook)
                 {
--- NOTEZ.CMD/Program.cs
+++ NOTEZ.CMD/Program.cs
@@ -11,35 +11,284 @@ namespace NOTEZ.CMD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\t\tДобро пожаловать в приложение NOTEZ!\t\t");
+            var userController = new UserController();
 
-            Console.WriteLine("Введите имя пользователя:");
-            var nameUser = Console.ReadLine();
-            var testUser = new UserController(nameUser);
+            while(MainMenu(ref userController)) { }
 
-            Console.WriteLine("Введите название блокнота:");
-            var titleNotebook1 = Console.ReadLine();
-            testUser.CreateNotebook(titleNotebook1);
-
-            Console.WriteLine("Введите название заметки:");
-            var titleNote1 = Console.ReadLine();
-            testUser.CreateNote(titleNotebook1, titleNote1);
-
-            testUser.Save();
-
-            Console.Clear();
-
-            Console.WriteLine($"Пользователь: {testUser.User}\n");
-            foreach (var notebook in testUser.User.Notebooks)
-            {
-                Console.WriteLine($"\tБлокнот: {notebook}\n");
-                foreach (var note in notebook.Notes)
-                {
-                    Console.WriteLine($"\t\tЗаметка: {note}\n");
-                }
-            }
+            while(UserMenu(ref userController)) { }
 
             Console.ReadLine();
         }
+
+        /// <summary>
+        /// Создание Главного меню.
+        /// </summary>
+        /// <param name="userController"> Контроллер пользователя. </param>
+        /// <returns> Флаг остановки. </returns>
+        static bool MainMenu(ref UserController userController)
+        {
+            Console.WriteLine("\t\tДобро пожаловать в приложение NOTEZ!\t\t\n");
+            Console.WriteLine("Для выбора пунктов Меню используйте соответствующие числа.\n");
+            Console.WriteLine("1 - Войти как зарегистрированный пользователь.");
+            Console.WriteLine("2 - Зарегистрироваться.");
+            Console.WriteLine("0 - Выйти из приложения");
+
+            string choice;
+            bool flagChoice = true;
+            bool flagStop = false;
+
+            while (flagChoice)
+            {
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.Clear();
+                        userController = UserAuthorization(ref flagStop);
+                        flagChoice = false;
+                        break;
+                    case "2":
+                        Console.Clear();
+                        userController = UserRegistration(ref flagStop);
+                        flagChoice = false;
+                        break;
+                    case "0":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Неккоректные данные.");
+                        break;
+                }
+            }
+
+            if(flagStop)
+            {
+                return false;
+            }
+
+            return true;    
+        }
+
+        /// <summary>
+        /// Создания Меню пользователя.
+        /// </summary>
+        /// <param name="userController"> Контроллер пользователя. </param>
+        /// <returns> Флаг остановки. </returns>
+        static bool UserMenu(ref UserController user)
+        {
+            Console.WriteLine($"\t\tВы вошли как {user.CurrentUser}\t\t");
+            Console.WriteLine("Для выбора пунктов Меню используйте соответствующие числа.\n");
+            Console.WriteLine("1 - Просмотреть все Блокноты.");
+            Console.WriteLine("2 - Создать новый Блокнот.");
+            Console.WriteLine("0 - Выйти из приложения.");
+
+            string choice;
+            bool flagChoice = true;
+            bool flagStop = false;
+
+            while (flagChoice)
+            {
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.Clear();
+                        NotebookViewMenu(ref user);
+                        flagChoice = false;
+                        break;
+                    case "2":
+                        Console.Clear();
+                        NotebookCreationMenu(ref user);
+                        flagChoice = false;
+                        break;
+                    case "0":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Неккоректные данные.");
+                        break;
+                }
+            }
+
+            if (flagStop)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Регистрация пользователя.
+        /// </summary>
+        /// <param name="isStop"> Флаг остановки. </param>
+        /// <returns> Контроллер пользователя. </returns>
+        static UserController UserRegistration(ref bool flagStop)
+        {
+            var userController = new UserController();
+
+            Console.WriteLine("\tЧтобы вернуться в меню, введите 0.");
+            Console.WriteLine("Введите имя пользователя:");
+
+            string userName;
+
+            while (true)
+            {
+                userName = Console.ReadLine();
+
+                if(userName == "0")
+                {
+                    Console.Clear();
+                    return userController;
+                }
+                else if(userController.UserSearch(userName))
+                {
+                    Console.WriteLine("Такой пользователь уже существует. Введите другое имя пользователя:");
+                }
+                else
+                {
+                    userController.CreateUser(userName);
+                    Console.Clear();
+                    flagStop = true;
+                    return userController;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Авторизация пользователя.
+        /// </summary>
+        /// <param name="isStop"> Флаг остановки. </param>
+        /// <returns> Контроллер пользователя. </returns>
+        static UserController UserAuthorization(ref bool flagStop)
+        {
+            var userController = new UserController();
+
+            Console.WriteLine("\tЧтобы вернуться в меню, введите 0.");
+            Console.WriteLine("Введите имя пользователя:");
+
+            string userName;
+
+            while (true)
+            {
+                userName = Console.ReadLine();
+
+                if (userName == "0")
+                {
+                    Console.Clear();
+                    return userController;
+                }
+                else if (userController.UserSearch(userName))
+                {
+                    userController.SetCurrentUser(userName);
+                    Console.Clear();
+                    flagStop = true;
+                    return userController;
+                }
+                else
+                {
+                    Console.WriteLine("Пользователь не найден. Введите другое имя пользователя:");
+                }
+            }         
+        }
+
+        /// <summary>
+        /// Меню просмотра и выбора Блокнотов.
+        /// </summary>
+        /// <param name="flagStop"> Флаг остановки. </param>
+        /// <param name="user"> Контроллер пользователя. </param>
+        static void NotebookViewMenu(ref UserController user)
+        {
+            Console.WriteLine("Для выбора Блокнота введите его название.");
+            Console.WriteLine("Для возврата в предыдущее меню введите 0.");
+
+            foreach (var notebook in user.CurrentUser.Notebooks)
+            {
+                Console.WriteLine($"Блокнот: {notebook}\n");
+                Console.WriteLine($"\tЗаметки:");
+                foreach (var note in notebook.Notes)
+                {
+                    Console.WriteLine($"\t\t{note}\n");
+                }
+            }
+
+            string choice;
+            bool flagStop = false;
+
+            while(true)
+            {
+                choice = Console.ReadLine();
+
+                if(choice == "0")
+                {
+                    Console.Clear();
+                    break;
+                }
+
+                foreach(var notebook in user.CurrentUser.Notebooks)
+                {
+                    if(choice == notebook.Title)
+                    {
+                        Console.Clear();
+                        flagStop = true;
+                        break;
+                    }
+                }
+
+                if(flagStop)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Неверное название Блокнота. Повторите попытку ввода:");
+            }
+        }
+
+        /// <summary>
+        /// Меню создания нового Блокнота.
+        /// </summary>
+        /// <param name="flagStop"> Флаг остановки. </param>
+        /// <param name="user"> Контроллер пользователя. </param>
+        static void NotebookCreationMenu(ref UserController user)
+        {
+            Console.WriteLine("Для возврата в предыдущее меню введите 0.");
+            Console.WriteLine("Введите название нового Блокнота.");
+
+            string notebookTitle;
+            bool flagStop = false;
+
+            while(true)
+            {
+                notebookTitle = Console.ReadLine();
+
+                if (notebookTitle == "0")
+                {
+                    Console.Clear();
+                    break;
+                }
+
+                foreach (var notebook in user.CurrentUser.Notebooks)
+                {
+                    if(notebookTitle == notebook.Title)
+                    {
+                        flagStop = true;
+                        break;
+                    }
+                }
+
+                if (!flagStop)
+                {
+                    user.CreateNotebook(notebookTitle);
+                    break;
+                }
+                
+                Console.WriteLine("Блокнот с таким именем уже существует. Введите другое имя Блокнота:");
+            }         
+        }
+
+        // TODO: Написать метод отображения конкретного Блокнота и создания новой заметки
+        // TODO: Написать метод отображение/редактирования заметки
     }
 }
